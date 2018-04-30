2b6d6e4756c99a535a7959e2c22cb475a2787822
iceland101113 <r02521115@ntu.edu.tw>
2018-04-30 20:21:04 +0800

    create post model

---
+++ db/migrate/20180430121522_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
