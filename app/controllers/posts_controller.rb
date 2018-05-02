eee472101edfff5ff86412577a86e79f08e5d7eb
iceland101113 <r02521115@ntu.edu.tw>
2018-05-02 18:03:39 +0800

    add image uploader and post create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "post was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "post was failed to create"
+      render :new
+    end
 
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :authority)
+  end
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_many :categorizations
   has_many :catgories, through: :categorizations
+
+  mount_uploader :image, ImageUploader
 end
