0c51a57f05f2b97aa33cf80c029f852d5d078a64
iceland101113 <r02521115@ntu.edu.tw>
2018-05-07 15:54:46 +0800

    setup reply routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :replies, only: [:create, :update, :destroy]
+  end
   
   root "posts#index"
 
