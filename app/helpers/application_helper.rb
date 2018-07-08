6785dbc8e0aeb311364f80e0677ccfe31e52654d
iceland101113 <r02521115@ntu.edu.tw>
2018-07-08 19:17:16 +0800

    change view count link function to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,12 @@ module ApplicationHelper
 
       end 
   end
+
+  def icon_link_to(label, path, icon)
+    link_to path do
+      content_tag :span, class: "glyphicon glyphicon-#{icon}" do
+        ' '+label
+      end
+    end
+  end
 end
